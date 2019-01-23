d333cb0f0c406cdb9ff6845256f54c3d0c1d6e7e
MrT3313 <rturgeon13@gmail.com>
2019-01-23 10:03:56 -0600

    final commit -- added branch for baseball version (html / css / js master project

--- assignments/this.js
+++ assignments/this.js
@@ -4,39 +4,11 @@
 * 1. GLOBAL BINDING
     -When in global scope -- 'this' = window/console object
 
-    -EX -- console.log(this);
 * 2. IMPLICIT BINDING
     -When a function is called by a preceding dot / calling method -- 'this' = [object LEFT of dot]
 
-    -EX -- 
-        const person = {
-            greeting: 'whats gucci fam?',
-            name: 'bob',
-            speak: function(otherName) {
-                console.log(`yo ${otherName} its ${this.name}, ${this.greeting}`)
-            }
-        }
-
-        person.speak('jacob');
-
 * 3. NEW BINDING
     -When a constructor function is used -- 'this' = specific instance of the object that is created & returned by constructor function
-
-
-    -EX --
-        function Person(name) {
-            this.greeting = 'whats gucci fam?',
-            this.name = name,
-            this.speak = function(otherName) {
-                console.log(`yo ${otherName}, its ${this.name}, ${this.greeting}`)
-            }
-        }
-
-        const BATMAN = new Person('BATMAN');
-        const SPIDERMAN = new Person('SPIDERMAN');
-
-        BATMAN.speak('JOKER');
-        SPIDERMAN.speak('GREEN GOBLIN');
 
 * 4. EXPLICIT BINDING
         -Override where 'this' is pointing        
@@ -44,23 +16,6 @@
             .call
             .apply
 
-        - EX --
-            function Person(name) {
-            this.greeting = 'whats gucci fam?',
-                this.name = name,
-                this.speak = function(otherName) {
-                console.log(`yo ${otherName}, its ${this.name}, ${this.greeting}`)
-            }
-            }
-
-            const BATMAN = new Person('BATMAN');
-            const SPIDERMAN = new Person('SPIDERMAN');
-
-            BATMAN.speak.call(SPIDERMAN);
-            SPIDERMAN.speak.apply(BATMAN);
-
-            BATMAN.speak('JOKER');
-            SPIDERMAN.speak('GREEN GOBLIN');
 *
 * write out a code example of each explanation above
 */
@@ -68,15 +23,54 @@
 // Principle 1
 
 // code example for Window Binding
+    console.log(this);
 
 // Principle 2
 
 // code example for Implicit Binding
+    const person = {
+        greeting: 'whats gucci fam?',
+        name: 'bob',
+        speak: function(otherName) {
+            console.log(`yo ${otherName} its ${this.name}, ${this.greeting}`)
+        }
+    }
+
+    person.speak('jacob');
 
 // Principle 3
 
 // code example for New Binding
+    function Person(name) {
+        this.greeting = 'whats gucci fam?',
+        this.name = name,
+        this.speak = function(otherName) {
+            console.log(`yo ${otherName}, its ${this.name}, ${this.greeting}`)
+        }
+    }
+
+    const BATMAN = new Person('BATMAN');
+    const SPIDERMAN = new Person('SPIDERMAN');
+
+    BATMAN.speak('JOKER');
+    SPIDERMAN.speak('GREEN GOBLIN');
 
 // Principle 4
 
 // code example for Explicit Binding
+    function Person(name) {
+        this.greeting = 'whats gucci fam?',
+            this.name = name,
+            this.speak = function(otherName) {
+            console.log(`yo ${otherName}, its ${this.name}, ${this.greeting}`)
+        }
+    }
+
+    const BATMAN = new Person('BATMAN');
+    const SPIDERMAN = new Person('SPIDERMAN');
+
+    BATMAN.speak.call(SPIDERMAN);
+    SPIDERMAN.speak.apply(BATMAN);
+
+    BATMAN.speak('JOKER');
+    SPIDERMAN.speak('GREEN GOBLIN');
