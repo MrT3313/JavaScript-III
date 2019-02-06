21f22bd9332d6bd643f7df0d14b479fa94626515
MrT3313 <rturgeon13@gmail.com>
2019-02-06 14:39:12 -0600

    structural cleanup

--- definedFunctions/create/newHalfInning.js
+++ definedFunctions/create/newHalfInning.js
@@ -2,4 +2,16 @@
     const newHalfInning = function () {
         let ACTIVE_halfInning = new HalfInningObject ({})
         return ACTIVE_halfInning
-    }    
+    }
+
+//**// Setup Inning
+const inningSetup = function (inningNumber) {
+    if (inningNumber === 18) {
+        // GAME OVER
+        console.log('game OVER')
+    } else {
+        newHalfInning()
+        console.log('--CREATED NEW ACTIVE HALF INNING--')
+        console.log(todaysGame.innings[0])  
+    }
+}
