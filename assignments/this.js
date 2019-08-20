a9a41e6a768b814d47f0ba1ec4cc5f684cbb5fab
MrT3313 <rturgeon13@gmail.com>
2019-08-20 13:07:22 -0500

    this.js completed

--- assignments/this.js
+++ assignments/this.js
@@ -16,11 +16,39 @@
 // Principle 2
 
 // code example for Implicit Binding
-
+const myObj = {
+    greeting: 'Hello',
+    sayHello: function(name) {
+        console.log(`${this.greeting} my name is ${name}`);
+    }
+}
+myObj.sayHello('Ryan')
 // Principle 3
 
 // code example for New Binding
+function CordialPerson(greeter) {
+    this.greeting =  'Hello';
+    this.greeter = greeter;
+    this.speak= function() {
+        // - V1 - // 
+            // console.log(this.greeting + this.greeter)
+        // - V2 - // 
+            return(`${this.greeting} ${this.greeter}`)
+    }
+}
+
+const jerry = new CordialPerson('Newman')
+const newman = new CordialPerson('Jerry')
+
+console.log(jerry.speak())
+console.log(newman.speak())
 
 // Principle 4
 
 // code example for Explicit Binding
+
+const marko = new CordialPerson('polo')
+const polo = new CordialPerson('marko')
+
+console.log(marko.speak.call(polo))
+console.log(polo.speak.apply(marko))
